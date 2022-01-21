90263a22003b81a58b62209cc2bee899d837b447
Kovarik <DENN10596@LAHCG.COM>
2022-01-21 08:25:28 -0700

    Added dependencies

--- setup.bat
+++ setup.bat
@@ -2,5 +2,13 @@
 ECHO OFF
 :: Update PIP
 call py -m pip install --upgrade pip
+:: Install win32com
+call py -m pip install pywin32
 :: Install Pandas
 call py -m pip install pandas
+:: Install openpyxl
+call py -m pip install openpyxl
+:: Install xlrd
+call py -m pip install xlrd
+:: Install tqdm
+call py -m pip install tqdm
